d11c539137e395f4a2c1027af88a1bbe76cf7371
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2020-12-11 08:22:50 +0900

    add use-package template for elisp.

---
+++ EmacsLisp/use-package-template.el
@@ -0,0 +1,47 @@
+;;; #FILENAME# --- Describe the File -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package #FILENAME# :no-require t :ensure t
+  ;; Package Loading Section.
+  ;;:disabled
+  ;;:defer
+  ;;:demand
+
+  ;; Package Dependency Section.
+  ;;:ensure-system-package
+  ;;:load-path
+  ;;:quelpa
+  ;;:pin
+  ;;:requires
+  ;;:after
+
+  ;; Package Mode Section.
+  ;;:delight
+  ;;:interpreter
+  ;;:hook
+  ;;:magic
+  ;;:magic-fallback
+  ;;:mode
+
+  ;; Package Command Section.
+  ;;:commands
+  ;;:bind
+  ;;:bind-key
+
+  ;; Package Custom Section.
+  ;;:custom
+  ;;:custom-face
+
+  ;; Package Configuration Section.
+  ;;:if :when :unless
+  ;;:defines
+  ;;:functions
+  ;;:preface
+  ;;:init
+  ;;:config
+  )
+
+(provide '#FILENAME#)
+
+;;; #FILENAME# ends here
